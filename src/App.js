e22b60b8c78c8fc7084516c1351f35015f4e35fb
HongJeHyeop <jhh951217@naver.com>
2023-06-22 16:04:44 +0900

    230622_create_introduction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 
 import Header from './components/Header';
+import Introduction from './components/Introduction'
 
 function App() {
     return (
         <div className="App">
             <Header/>
+            <Introduction/>
         </div>
     );
 }
---
+++ src/components/Introduction.js
@@ -0,0 +1,20 @@
+import "../css/introduction.css";
+
+function Introduction() {
+    return (
+        <div id="intro">
+            <div id="intro-wrap">
+                <h2>INTRODUCTION</h2>
+                <div className="contents">
+                <span>안녕하세요 백엔드 신입 개발자 홍제협입니다. <br/>
+                    저는 10년간 악기를 전공하며 이 분야에서 전문가라 불릴수 있는 자리에 올랐습니다. <br/>
+                    이제는 개발이라는 새로운 분야에서 전문가가 되어보고자 합니다. <br/>
+                    다시한번 꾸준히 성실히 노력하겠습니다.
+                </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Introduction;
